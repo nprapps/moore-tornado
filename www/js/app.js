4435ffcd5dc548c0678515262582849930346fc8
Brian Boyer <bboyer@npr.org>
2013-05-21 17:27:32 -0400

    tweaks to zoom things

--- www/js/app.js
+++ www/js/app.js
@@ -2,26 +2,19 @@ $(document).ready(function(){
     mapbox.load(['npr.ok-moore-tornado'], function(data){
         window.m = mapbox.map('map');
         m.addLayer(data[0].layer);
-        m.setZoomRange(14, 18);
+        m.setZoomRange(12, 18);
         m.interaction.auto();
-        m.zoom(14);
-        m.center({ lat: 35.338, lon: -97.486 });
-        m.ui.zoomer.add();
-    
-        // var width = $(window).width();
-        // 
-        // if(width >= 960){
-        //     m.zoom(14);
-        //     m.center({ lat: 35.338, lon: -97.486 });
-        //     m.ui.zoomer.add();
-        // } else if (width >= 650){
-        //     m.zoom(13);
-        //     m.center({ lat: 35.338, lon: -97.486 });
-        //     m.ui.zoomer.add();
-        // } else {
-        //     m.zoom(12);
-        //     m.center({ lat: 35.338, lon: -97.486 });
-        // }
+        
+        var width = $(window).width();
+        
+        if(width > 768){
+            m.zoom(14);
+            m.center({ lat: 35.338, lon: -97.486 });
+            m.ui.zoomer.add();
+        } else {
+            m.zoom(13);
+            m.center({ lat: 35.338, lon: -97.486 });
+        }
 
         $("#about").click(function(){
             if($(".modal-body").children().length < 1 ) {
