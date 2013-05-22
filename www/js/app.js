c3b4b9de5948415b23f0c2f102b7d30275569bef
Brian Boyer <bboyer@npr.org>
2013-05-22 11:10:41 -0400

    prototyped zoomlens. works like a charm, closes #46

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
         minZoom:12,
         maxZoom:19
     });
-    map.addLayer(L.mapbox.tileLayer('npr.map-g7ewv5af,npr.ok-moore-tornado-satellite'));
+    map.addLayer(L.mapbox.tileLayer('npr.map-g7ewv5af'));
     //map.addLayer(L.mapbox.tileLayer('http://localdev.npr.org:20009/api/Project/ok-moore-tornado/'));
     
     L.control.scale().addTo(map);
@@ -14,6 +14,27 @@ $(document).ready(function(){
         map.setView([35.338, -97.486], 14);
     } else {
         map.setView([35.338, -97.486], 13);
+    }
+    
+    var zoommap = L.mapbox.map('zoommap', 'npr.map-g7ewv5af,npr.ok-moore-tornado-satellite', {
+        fadeAnimation: false,
+        zoomControl: false,
+        attributionControl: false
+    });
+
+    var zl = document.getElementById('zoomlens');
+
+    map.on('mousemove', update);
+    map.on('zoomend', zoom);
+
+    function zoom(e) {
+        if (zoommap._loaded) zoommap.setZoom(map.getZoom());
+    }
+
+    function update(e) {
+        zl.style.top = ~~e.containerPoint.y-100 + 'px';
+        zl.style.left = ~~e.containerPoint.x-100 + 'px';
+        zoommap.setView(e.latlng, map.getZoom(), true);
     }
 
     $('#about').click(function(){
